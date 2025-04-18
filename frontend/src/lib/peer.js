853b140b6326256dadc582cde2a9b49d663fb77d
martise027 <takesaday.b@gmail.com>
2025-04-18 18:50:12 +0200

    10

--- frontend/src/lib/peer.js
+++ frontend/src/lib/peer.js
@@ -45,8 +45,8 @@ export class PeerConnection {
     
 
     createSdpExchange() { // WebSocket with listeners for exchanging SDP offers and answers
-        let protocol = window.location.protocol === "https:" ? "wss" : "ws";
-        let ws = new WebSocket(`${protocol}://${window.location.host}/api/matchmaking`);
+        //let protocol = window.location.protocol === "https:" ? "wss" : "ws";
+        let ws = new WebSocket(import.meta.env.WS_LINK);
         //let ws = new WebSocket(`${protocol}://localhost:8000/api/matchmaking`);
 
         ws.addEventListener("message", (event) => {
